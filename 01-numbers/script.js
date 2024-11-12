a5129a49ab54579dd66a7c51d9a5c69f8f89588d
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-12 16:45:14 +0100

    fix: completata prima pagina

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -26,9 +26,11 @@ const numThreeMinusNumOne = numThree - numOne;
 console.log(numThreeMinusNumOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = ;
+const sum = numOne + numTwo + numThree;
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = NaN;
+
+let sommatotale = numOne + numTwo + numThree
+const numBytes = sommatotale / numOne;
 console.log(numBytes); // Output: 7
